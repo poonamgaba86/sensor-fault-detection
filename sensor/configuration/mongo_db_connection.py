cb296445a4032e663ac56577f785e1142aab6065
poonamgaba86 <poonamgaba86@gmail.com>
2023-01-24 14:11:30 +0530

    Update mongo_db_connection.py

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -9,7 +9,7 @@ class MongoDbClient:
     def __init__(self,database_name=DATABASE_NAME):
         try:
             if MongoDbClient.client is None:
-                mongo_db_url="mongodb+srv://Poonam:datascience123@cluster0.g2gqd10.mongodb.net/test"
+                mongo_db_url="mongodb+srv://Poonam:<password>@cluster0.g2gqd10.mongodb.net/test"
                 MongoDbClient.client=pymongo.MongoClient(mongo_db_url,tlsCAFile=ca)
             self.client=MongoDbClient.client
             self.database=self.client[database_name]
