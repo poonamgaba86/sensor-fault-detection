bc8cc24ebbb914cca97585399733491da8f3ed80
poonam Gaba <poonamgaba86@gmail.com>
2023-02-20 10:40:40 +0530

    completed with AWS

--- sensor/constant/s3_bucket.py
+++ sensor/constant/s3_bucket.py
@@ -1,2 +1,2 @@
-TRAINING_BUCKET_NAME = "scania-sensor-pipeline"
-PREDICTION_BUCKET_NAME = "sensor-datasource"
+TRAINING_BUCKET_NAME = "aps-sensor-pipeline-ml"
+PREDICTION_BUCKET_NAME = "aps-sensor-datasource"
